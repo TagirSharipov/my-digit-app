d433262c15b46102d90286242a7130dc51cf48ba
Tagir Sharipov <tsharipov@keybiz.it>
2025-04-10 21:35:04 +0200

    added actions

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -1,11 +1,12 @@
 import { Link, useSearchParams } from 'react-router';
+import type { Route } from '../routes/+types/login';
 
 export default function AuthForm() {
   const [searchParams] = useSearchParams();
 
   const mode = searchParams.get('mode');
   const isLogin = mode === 'login';
-  const isSignup = mode === 'register';
+  const isSignup = mode === 'signup';
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold"> {isSignup ? 'Register' : 'Login'}</h1>
@@ -13,13 +14,15 @@ export default function AuthForm() {
       <form className="flex flex-col items-center" method="post">
         <input
           type="text"
-          placeholder="Username"
+          placeholder="Email"
           className="mb-2 p-2 border rounded"
+          name="email"
         />
         <input
           type="password"
           placeholder="Password"
           className="mb-2 p-2 border rounded"
+          name="password"
         />
         <button type="submit" className="p-2 bg-blue-500 text-white rounded">
           {isSignup ? 'Register' : 'Login'}
@@ -31,7 +34,7 @@ export default function AuthForm() {
             I have an account
           </Link>
         ) : (
-          <Link to="/login?mode=register" className="text-blue-500">
+          <Link to="/login?mode=signup" className="text-blue-500">
             Sign up
           </Link>
         )}
@@ -39,3 +42,17 @@ export default function AuthForm() {
     </div>
   );
 }
+/* 
+/* export async function clientAction({ request }: Route.ClientActionArgs) {
+  let formData = await request.formData();
+  let title = formData.get('title');
+  console.log('Form submitted with title:', title);
+  return;
+}
+ 
+
+export async function action({ request }: Route.ActionArgs) {
+  let formData = await request.formData();
+  let title = formData.get('title');
+  console.log('Form submitted with title:', title);
+} */
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,6 +1,7 @@
 import type { Route } from './+types/home';
 import { Welcome } from '../welcome/welcome';
 import LoginForm from '~/components/AuthForm';
+import { redirect } from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,3 +13,34 @@ export function meta({}: Route.MetaArgs) {
 export default function Login() {
   return <LoginForm />;
 }
+
+export async function action({ request }: Route.ActionArgs) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode');
+  if (mode !== 'login' && mode !== 'signup') {
+    throw new Response('Invalid mode', { status: 422 });
+  }
+
+  const formData = await request.formData();
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  console.log('Username:', password);
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  console.log('Response:', response);
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+  if (!response.ok) {
+    throw new Response('Something went wrong', { status: 500 });
+  }
+  return redirect('/welcome');
+}
+
+//TODO validation, error handling, protected route
